756a5a6551011b95aa95c504d528ea081633f57c
Sneha Kasuganti <snehakasuganti@Snehas-MacBook-Pro.local>
2025-07-15 21:56:34 -0700

    Folder open and close

--- src/folder-file-structure/FolderFileStructureClone.js
+++ src/folder-file-structure/FolderFileStructureClone.js
@@ -12,17 +12,31 @@ import { mockFolderStructure } from "./mockFileData";
  */
 const FolderFileStructureClone = () => {
   const [data, setData] = useState(mockFolderStructure);
+  // Folder open - close status state
+  const [folderStatus, setFolderStatus] = useState({});
+
+  const updateFolderStatus = (folderId) => {
+    setFolderStatus((prev) => ({ ...prev, [folderId]: !!!prev[folderId] }));
+  };
 
   const ListStructure = ({ nodes }) => {
     return (
       <div className="node-list-container">
-        {nodes.map(({id, name, isFolder, children}) => {
+        {nodes.map(({ id, name, isFolder, children }) => {
           return (
             <div className="node-element" key={id}>
+              {isFolder && (
+                <button
+                  className="node-expand"
+                  onClick={() => updateFolderStatus(id)}
+                >
+                  {folderStatus[id] ? "-" : "+"}
+                </button>
+              )}
               <span>{name}</span>
-              {
-                isFolder && children && <ListStructure nodes={children} />
-              }
+              {isFolder && children && folderStatus[id] && (
+                <ListStructure nodes={children} />
+              )}
             </div>
           );
         })}
