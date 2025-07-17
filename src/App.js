9ccd92a1f06f4c0baeb911565c30b008347dd59e
Sneha Kasuganti <snehakasuganti@Snehas-MacBook-Pro.local>
2025-07-16 18:48:02 -0700

    Autocomplete search - skeleton and fetching + displaying results

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import QRCodeCardComponent from "./scan-qr/QRCodeCardComponent";
 import FolderFileStructureClone from "./folder-file-structure/FolderFileStructureClone";
+import AutoComplete from "./auto-complete/AutoComplete";
 
 const AppLayout = () => {
     return (<div className="components-container">
         {/* <QRCodeCardComponent/> */}
-        <FolderFileStructureClone />
+        {/* <FolderFileStructureClone /> */}
+        <AutoComplete />
     </div>);
 }
 
---
+++ src/auto-complete/AutoComplete.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import "./autoComplete.css";
+
+/**
+ * Container
+ *  - Heading
+ *  - Search Bar
+ *  - fetch data upon search/ type
+ *  - Display results
+ *  - Implement debouncing for optimization
+ *  - Implement caching for optimization
+ *  - Additional
+ *    - close the search results section on blur and show on focus
+ *    - Keyboard navigation
+ * @returns Search component
+ */
+const AutoComplete = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [searchResults, setSearchResults] = useState();
+
+  const fetchResults = async () => {
+    if (!inputValue){
+        setSearchResults();
+        return;
+    }
+    const apiResults = await fetch(
+      `https://dummyjson.com/recipes/search?q=${inputValue}`
+    );
+    const resultData = await apiResults.json();
+
+    console.log(resultData);
+    setSearchResults(resultData?.recipes);
+  };
+  useEffect(() => {
+    fetchResults();
+  }, [inputValue]);
+
+  return (
+    <div className="search-container">
+      <img
+        className="search-logo"
+        src="https://www.google.com/logos/doodles/2025/mothers-day-2025-may-25-6753651837110683-2x.png"
+        alt="logo"
+      />
+      <div className="search-bar-container">
+        <input
+          value={inputValue}
+          className="search-bar"
+          onChange={(e) => {
+            console.log(e);
+            setInputValue(e.target.value);
+          }}
+        />
+        {searchResults?.length > 0 && (
+          <div className="search-results-container">
+            <ul className="search-results-list">
+              {searchResults?.map((result) => {
+                return <li key={result.id}>{result.name}</li>;
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AutoComplete;
